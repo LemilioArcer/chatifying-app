20ba7b85310eb06bf7fb97791764c74af57f6731
Arce <emilioarce1285@gmail.com>
2026-04-21 19:10:35 -0600

    VAmos

--- src/socket.js
+++ src/socket.js
@@ -1,10 +1,14 @@
 import { io } from 'socket.io-client';
 
-const URL = 'https://chatifying-server-production.up.railway.app';
+const RAILWAY_URL = 'https://chatifying-server-production.up.railway.app';
+
+const URL = process.env.NODE_ENV === 'production' ? RAILWAY_URL : 'http://localhost:3000';
 
 export const socket = io(URL, {
     auth: {
         serverOffset: 0,
+        ackTimeout: 10000,
+        retries: 3,
     },
     withCredentials: true 
 });
