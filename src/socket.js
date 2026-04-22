87124f9672ce1dbec6c5d3162dcaa8eebcda5d18
Arce <emilioarce1285@gmail.com>
2026-04-21 18:45:18 -0600

    Kill

--- src/socket.js
+++ src/socket.js
@@ -1,14 +1,10 @@
 import { io } from 'socket.io-client';
 
-const RAILWAY_URL = 'https://chatifying-server-production.up.railway.app';
-
-const URL = process.env.NODE_ENV === 'production' ? RAILWAY_URL : 'http://localhost:3000';
+const URL = 'https://chatifying-server-production.up.railway.app';
 
 export const socket = io(URL, {
     auth: {
         serverOffset: 0,
-        ackTimeout: 10000,
-        retries: 3,
     },
     withCredentials: true 
 });
